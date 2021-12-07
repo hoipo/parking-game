1cc3db1f5a2e2d426f46ed4598f115f081289664
bobo <578344975@qq.com>
2021-12-07 23:20:04 +0800

    feat: remove the mars icon

--- src/components/partners/index.tsx
+++ src/components/partners/index.tsx
@@ -1,13 +1,13 @@
 import { Col, Row } from 'antd'
 import styles from './styles.module.less'
-import marsIcon from './images/partnet_mars.png'
+// import marsIcon from './images/partnet_mars.png'
 const Partners = () => {
     return (
         <div id='partners' className={styles.partners}>
             <div className={styles.title}><span>Partners</span></div>
             <Row gutter={80} justify='center' align='middle'>
                 <Col className={styles.item}>
-                    <a href="https://marsecosystem.com/home" target='_blank' rel="noreferrer"><img className={styles.mars} src={marsIcon} alt="mars" /></a>
+                    {/* <a href="https://marsecosystem.com/home" target='_blank' rel="noreferrer"><img className={styles.mars} src={marsIcon} alt="mars" /></a> */}
                 </Col>
             </Row>
         </div>
